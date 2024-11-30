8b38c0984467d7024b2357aa1d7afd11ccbf5314
Inkashi <zabolotin.nikitka@mail.ru>
2024-11-30 09:34:05 +0500

    .start

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('')
+var temp = ref('Привет')
+const tg = window.Telegram.WebApp
+tg.expand()
+function msgTelegram() {
+  try {
+    tg.s
+  } catch (e) {
+    msg.value = e
+  }
+  console.log(data)
+  tg.close()
+}
+
+function sendMsg() {
+  tg.sendData(msg.value)
+}
+</script>
+<template>
+  <body>
+    <div class="remix">
+      <h2>{{ temp }}</h2>
+      <h1>{{ msg }}</h1>
+      <input v-model="msg" />
+      <button @click="sendMsg">Отправить</button>
+      <button class="btn btn-primary">Кнопка</button>
+    </div>
+    <button>Кнопка</button>
+  </body>
+</template>
+
+<style lang="scss" scoped>
+.remix {
+  width: max-content;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20rem;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/styles.scss'
+import './assets/main.css'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
